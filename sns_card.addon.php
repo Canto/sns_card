7910a4ea65831b58dd0cda575dfe51cc786fef9c
canto87-mac <canto87@naver.com>
2014-06-05 15:51:17 +0900

    기본 설정 추가

--- sns_card.addon.php
+++ sns_card.addon.php
@@ -9,25 +9,36 @@ if(!defined('__XE__')) exit();
 
 if($called_position == "before_display_content"){
 	$doc_src = Context::get('document_srl');
-	$oDocumentModel = &getModel('document');
-	$oDocument = $oDocumentModel->getDocument($doc_src);
-	$title = cut_str(strip_tags($oDocument->get('title')),70,'');
-	$content = cut_str(strip_tags($oDocument->get('content')),200,'');
-	$file_list = $oDocument->getUploadedFiles();
-	if($file_list){
-		$source = $file_list[0]->uploaded_filename;
-		if(strlen($source) >= 2 && substr_compare($source, './', 0, 2) === 0)
+	if($doc_src){
+		$oDocumentModel = &getModel('document');
+		$oDocument = $oDocumentModel->getDocument($doc_src);
+		$title = cut_str(strip_tags($oDocument->get('title')),70,'');
+		$content = cut_str(strip_tags($oDocument->get('content')),200,'');
+		$file_list = $oDocument->getUploadedFiles();
+		if($file_list)
 		{
-			$filePath = Context::get('request_uri') . substr($source, 2);
-
+			$source = $file_list[0]->uploaded_filename;
+			if(strlen($source) >= 2 && substr_compare($source, './', 0, 2) === 0)
+			{
+				$filePath = Context::get('request_uri') . substr($source, 2);
+			}
+		}else{
+			$filePath = $addon_info->logo;
 		}
 	}
+	else
+	{
+		$title = $addon_info->sitename;
+		$content = $addon_info->content;
+		$filePath = $addon_info->logo;
+	}
+
 	$meta = '<meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="'.$addon_info->twitter.'">
 <meta name="twitter:title" content="'.$title.'">
 <meta name="twitter:description" content="'.$content.'">
 <meta name="twitter:image:src" content="'.$filePath.'">
-<meta name="twitter:domain" content="'.Context::getSiteTitle().'">
+<meta name="twitter:domain" content="'.$addon_info->sitename.'">
 <meta property="og:title" content="'.$title.'">
 <meta property="og:type" content="article">
 <meta property="og:url" content="'.Context::get('current_url').'">
